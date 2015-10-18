0f8aa63a371316bd3660f9430f599d0dc633f3e3
alxcancado <alxcancado@gmail.com>
2015-10-18 19:10:00 +1300

    First Commit

---
+++ 3. Variables and types Playground/3. variables and types.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, Alex Lima Lopes Cancado"
+
+var fifiteen = 15
+var five = 5
+
+var sum = fifiteen + five
+
+var operation = (fifiteen * five) - sum
+
+var difference = fifiteen - five
+
+var division = fifiteen / five
+
+var a = 1, b = 2, c = 3
+
+let d = 4, e = 5, f = 6
+
+var double: Double
+
+var intt: Int = 423
+
+var hello: String = "Hello"
+
+var world: String = "World"
+
+var helloWorld = hello + " " + world
+
+var again = "\(hello) \(world)"
---
+++ 5. Functions Playground/5. func.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add ( num1 : Double, num2 : Double) -> Double {
+
+    return num1 + num2
+    
+}
+
+add( 1.1 , num2: 3.2)
+
+
+func substract (num1:Int, num2:Int) -> Int{
+    
+    return num1 + num2
+
+}
+
+substract(2, num2: 2)
+
+func multiply(num1:float_t, num2:float_t) -> float_t{
+    
+    return num1 * num2
+
+}
+
+multiply(4, num2: 5)
+
+func divide(num1:Double, num2:Double) -> Double{
+    
+    return num1 / num2
+}
+
+divide(5, num2: 2)
+
+
+
+
+
+
+
