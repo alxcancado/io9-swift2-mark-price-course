a0493deadec4d424c45563304d75f818a037d11e
alxcancado <alxcancado@gmail.com>
2015-10-18 19:59:44 +1300

    Add 2. RedBlueBirds folder

---
+++ 2. RedBlueBirds/RedBlueBirds/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  RedBlueBirds
+//
+//  Created by Alex Lima Lopes Cancado on 5/10/15.
+//  Copyright © 2015 Alex Lima Lopes Cancado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var BluButton: UIButton!
+    @IBOutlet weak var BlueBird: UIImageView!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var redBird: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hideBlue(sender: AnyObject) {
+        BlueBird.hidden = true
+    }
+   
+    @IBAction func hideRed(sender: AnyObject) {
+        redBird.hidden = true
+    }
+    
+}
