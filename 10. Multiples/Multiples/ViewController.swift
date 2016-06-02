64a4e334c5c4ebd861b8461478c8494e27f872b9
alxcancado <alxcancado@gmail.com>
2016-06-02 17:17:42 +1200

    app working as should be; added button: back

--- 10. Multiples/Multiples/ViewController.swift
+++ 10. Multiples/Multiples/ViewController.swift
@@ -10,14 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // properties
+    var multipleNumber = 0
+    var counter = 0
+    
+    // Outlets
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    // Actions
+    @IBAction func onPlayButtonPressed(sender: UIButton!){
+        
+        //self.inputTextField.keyboardType = UIKeyboardType.NumberPad
+        
+        if inputTextField.text != nil && inputTextField.text != "" {
+            
+            logo.hidden = true
+            inputTextField.hidden = true
+            playButton.hidden = true
+            
+            backButton.hidden = false
+            addButton.hidden = false
+            resultsLabel.hidden = false
+            
+            multipleNumber = Int(inputTextField.text!)!
+        }
     }
+    
+    
+    @IBAction func onAddButtonPressed(sender: UIButton!){
+        
+        resultsLabel.text = "\(counter) + \(multipleNumber) = \(counter + multipleNumber)"
+        counter = counter + multipleNumber
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onBackButtonPressed(sender: UIButton!){
+        // resets
+        logo.hidden = false
+        inputTextField.hidden = false
+        playButton.hidden = false
+        
+        backButton.hidden = true
+        addButton.hidden = true
+        resultsLabel.hidden = true
+        
+        inputTextField.text = ""
+        resultsLabel.text = "press ADD"
+        multipleNumber = 0
+        counter = 0
+        
+    }
+    
+    
+    
+    // trick to hide the status bar
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
 
