86951de1fe7937ed3f1fa884eb1543ae6887c78c
alxcancado <alxcancado@gmail.com>
2016-06-10 08:56:30 +1200

    25. pokepet: UIImage animation

--- 25. pokepet/pokepet/ViewController.swift
+++ 25. pokepet/pokepet/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        var imageArray = [UIImage]()
+        
+        for x in 1...4 {
+            let img = UIImage(named: "idle\(x).png")
+            imageArray.append(img!)
+        }
+        
+        monsterImage.animationImages = imageArray
+        monsterImage.animationDuration = 0.6
+        monsterImage.animationRepeatCount = 0
+        monsterImage.startAnimating()
+        
+    
     }
 
 
