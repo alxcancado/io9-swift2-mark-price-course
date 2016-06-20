2b013048f868f540660c602cb95937e39fad4bd8
alxcancado <alxcancado@gmail.com>
2016-06-20 14:41:09 +1200

    32. Animal Sounds: on animal icon touch, shows the animal pcture screen

--- 32. Animal Sounds/Animal Sounds/ImageSoundViewController.swift
+++ 32. Animal Sounds/Animal Sounds/ImageSoundViewController.swift
@@ -1,17 +1,15 @@
 //
-//  GameViewController.swift
+//  ImageSoundViewController.swift
 //  Animal Sounds
 //
-//  Created by Alex Lima Lopes Cancado on 18/06/16.
+//  Created by Alex Lima Lopes Cancado on 20/06/16.
 //  Copyright © 2016 Alex Lima Lopes Cancado. All rights reserved.
 //
 
 import UIKit
 
-class GameViewController: UIViewController {
-    //reference to the BlueViewController
-    var imageSoundVC = ImageSoundViewController()
-    
+class ImageSoundViewController: UIViewController {
+
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
@@ -21,36 +19,23 @@ class GameViewController: UIViewController {
     }
     
 
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func loadImageSoundView(sender: AnyObject){
-        imageSoundVC = ImageSoundViewController(nibName: "ImageSoundViewController", bundle: nil)
-        // its initialised already by the BlueViewController itself, so
-        // imageSoundVC = ImageSoundViewController(image: "cow1.jpg")
-        self.presentViewController(imageSoundVC, animated: true, completion: nil)
-        
-    }
-
     
     @IBAction func loadPreviousVC(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
-    
-    
+
     /*
     // MARK: - Navigation
 
